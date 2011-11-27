d03a735e8fa02fa1425bd8896332d446c01f431f
David Walter Seikel <won_fang@yahoo.com.au>
2011-11-28 00:05:12 +1000

    No need to deal with ownership and perms, the install script does that now.

--- go_live.sh
+++ go_live.sh
@@ -7,13 +7,6 @@ do
     j=$(printf "sim%02d" $i)
     if [ -e "$OSPATH/config/$j" ]
     then
-	sudo chown -R opensim:opensim $OSPATH/config/$j
 	sudo ln -s $OSPATH/config/$j/opensim-monit.conf /etc/monit/conf.d/$j.conf
     fi
 done
-
-sudo chmod 755 /var/log/opensim
-sudo chmod 755 /var/run/opensim
-sudo chown -R opensim:opensim $OSPATH/opensim-0.7.1.1-infinitegrid-03
-sudo chown -R opensim:opensim $OSPATH/modules
-
