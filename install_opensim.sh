c26c48c23e6f45ac26cf567d09f12e995098426c
David Walter Seikel <won_fang@yahoo.com.au>
2012-02-06 12:15:52 +1000

    chmod on the correct directory.

--- install_opensim.sh
+++ install_opensim.sh
@@ -125,7 +125,7 @@ sudo chown -R opensim:opensim $OSPATH
 sudo chmod -R a-x $OSPATH
 sudo chmod -R a+X $OSPATH
 sudo chmod -R g+w $OSPATH
-sudo chmod -R a+x $OSPATH/*.sh
+sudo chmod -R a+x $OSPATH/setup/*.sh
 sudo chmod a+x $OSPATH/setup/start-sim
 sudo chmod a+x $OSPATH/setup/start-sim-screen
 
